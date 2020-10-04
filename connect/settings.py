fd4e99ca702b1e28ea4aac00234e1f6dd8753941
Manav Parmar <parmar.manav2000@gmail.com>
2020-10-04 16:53:57 +0530

    made it responsive

--- connect/settings.py
+++ connect/settings.py
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'connect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'social_media',
-        'USER': 'postgres',
-        'PASSWORD': 'Parmar.2000',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'social_media',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Parmar.2000',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
